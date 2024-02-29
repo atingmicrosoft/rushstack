f0939ddf464f5bf2556c8e59d57c0f1fa82e1906
atingmicrosoft <115047461+atingmicrosoft@users.noreply.github.com>
2024-02-29 14:53:09 -0800

    modified way of creating tls certificates and saving

--- vscode-extensions/rush-vscode-extension/src/extension.ts
+++ vscode-extensions/rush-vscode-extension/src/extension.ts
@@ -9,10 +9,15 @@ import { RushWorkspace } from './logic/RushWorkspace';
 import { RushProjectsProvider } from './providers/RushProjectsProvider';
 import { RushTaskProvider } from './providers/TaskProvider';
 import { RushCommandWebViewPanel } from './logic/RushCommandWebViewPanel';
+import { FileSystem } from '@rushstack/node-core-library';
+import { CertificateManager, type ICertificate } from '@rushstack/debug-certificate-manager';
+import { homedir } from 'node:os';
+import * as path from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
+  vscode.window.showInformationMessage('Rush Stack extension is now active!');
   context.subscriptions.push(
     vscode.commands.registerCommand('rushstack.selectWorkspace', async () => {
       await RushWorkspace.selectWorkspace();
@@ -60,6 +65,46 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 
     RushCommandWebViewPanel.initialize(context).reveal();
   }
+
+  const certificateManager: CertificateManager = new CertificateManager();
+
+  // logger.terminal.writeLine(`Untrusting existing CA certificate`);
+  // await certificateManager.untrustCertificateAsync(logger.terminal);
+
+  terminal.writeLine(`Obtaining a TLS certificate signed by a local self-signed Certificate Authority`);
+
+  const {
+    pemCaCertificate,
+    pemCertificate,
+    pemKey
+  }: ICertificate & {
+    pemCaCertificate?: string;
+  } = await certificateManager.ensureCertificateAsync(true, terminal);
+
+  if (!pemCertificate || !pemKey) {
+    throw new Error(`No certificate available, exiting.`);
+  }
+  terminal.writeLine(`Trusted TLS certificate successfully obtained`);
+
+  const unresolvedUserFolder: string = homedir();
+  const userProfilePath: string = path.resolve(unresolvedUserFolder);
+  if (!FileSystem.exists(userProfilePath)) {
+    throw new Error("Unable to determine the current user's home directory");
+  }
+
+  const serveDataPath: string = path.join(userProfilePath, '.rushstack');
+  FileSystem.ensureFolder(serveDataPath);
+
+  const caCertificatePath: string = path.join(serveDataPath, 'rushstack-ca.pem');
+  const certificatePath: string = path.join(serveDataPath, 'rushstack-serve.pem');
+  const keyPath: string = path.join(serveDataPath, 'rushstack-serve.key');
+
+  if (pemCaCertificate) {
+    await FileSystem.writeFileAsync(caCertificatePath, pemCaCertificate);
+  }
+
+  await FileSystem.writeFileAsync(certificatePath, pemCertificate);
+  await FileSystem.writeFileAsync(keyPath, pemKey);
 }
 
 // this method is called when your extension is deactivated
