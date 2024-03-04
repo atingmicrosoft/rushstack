e9bb67cf467ba2480fa6c06dff0f8eeb5825a1cb
atingmicrosoft <115047461+atingmicrosoft@users.noreply.github.com>
2024-03-04 15:30:14 -0800

    init new tls certification ui extension

---
+++ vscode-extensions/tls-certification-ui/src/extension.ts
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { CertificateManager } from '@rushstack/debug-certificate-manager';
+import { ConsoleTerminalProvider, Terminal } from '@rushstack/terminal';
+
+const consoleTerminalProvider: ConsoleTerminalProvider = new ConsoleTerminalProvider();
+
+export const terminal: Terminal = new Terminal(consoleTerminalProvider);
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+  //const extensionConfiguration: vscode.WorkspaceConfiguration =
+  //vscode.workspace.getConfiguration('tls-certification-ui');
+
+  // terminal.writeLine(`Extension configuration: ${JSON.stringify(extensionConfiguration)}`);
+
+  // const extensionLogLevel: LogLevel | undefined = extensionConfiguration.get('logLevel');
+  // if (extensionLogLevel) {
+  //   setLogLevel(extensionLogLevel);
+  // }
+  // vscode.window.showInformationMessage('TLS Cetification UI Extension is now active!');
+  terminal.writeLine('was here');
+  context.subscriptions.push(
+    vscode.commands.registerCommand('rushstack.ensureDebugCertificate', async () => {
+      terminal.writeLine('was here 2');
+      const manager: CertificateManager = new CertificateManager();
+      terminal.writeLine('was here 3');
+      const { pemCaCertificate, pemCertificate, pemKey } = await manager.ensureCertificateAsync(
+        true,
+        terminal
+      );
+
+      terminal.writeLine(`pemCaCertificate: ${pemCaCertificate}`);
+      terminal.writeLine(`pemCertificate: ${pemCertificate}`);
+      terminal.writeLine(`pemKey: ${pemKey}`);
+
+      if (pemCaCertificate && pemCertificate && pemKey) {
+        await vscode.window.showInformationMessage('Certificate successfully generated');
+      } else {
+        await vscode.window.showErrorMessage('Certificate was not generated');
+      }
+
+      await vscode.commands.executeCommand(
+        'rushstack.saveDebugCertificate',
+        pemCaCertificate,
+        pemCertificate,
+        pemKey
+      );
+    })
+  );
+}
+
+// this method is called when your extension is deactivated
+export function deactivate(): void {}
